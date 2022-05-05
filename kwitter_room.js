adc71ec6fb8bd03f8fd8de25b180d82d163a2c99
dhivyameena <94811372+dhivyameena@users.noreply.github.com>
2022-05-04 20:52:24 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAd3pwwM6jd3vTEZUgUMivfAJIhEtnbc5U",
+    authDomain: "kwitter-app-43e24.firebaseapp.com",
+    databaseURL: "https://kwitter-app-43e24-default-rtdb.firebaseio.com",
+    projectId: "kwitter-app-43e24",
+    storageBucket: "kwitter-app-43e24.appspot.com",
+    messagingSenderId: "910301731706",
+    appId: "1:910301731706:web:fa925fbadec7e0331cd4c3"
+  };
+  
+firebase.initializeApp(firebaseConfig);
